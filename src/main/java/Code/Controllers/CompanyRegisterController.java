ab6491a0a137d6114b955a9ba184c9d80a847ec2
CCXander <103416424+CCXander@users.noreply.github.com>
2022-05-21 17:57:59 +0300

    Company Register interface prototype

--- src/main/java/Code/Controllers/CompanyRegisterController.java
+++ src/main/java/Code/Controllers/CompanyRegisterController.java
@@ -1,4 +1,74 @@
 package Code.Controllers;
 
-public class CompanyRegisterController {//comment for checkout testing
+import Code.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class CompanyRegisterController {
+
+@FXML
+    private TextField usernameTextField;
+
+@FXML
+    private TextField passwordTextField;
+
+@FXML
+    private TextField companyPhoneTextField;
+
+@FXML
+    private TextField HQAddressTextField;
+
+    public String username;
+    public String password;
+    public String companyPhone;
+    public String HQAddress;
+
+    private Stage stage;
+    private Scene scene;
+
+    public void onRegisterButtonClick(ActionEvent event) throws IOException {
+
+        username="";
+        password="";
+        companyPhone="";
+        HQAddress="";
+
+        username = String.valueOf(usernameTextField.getText());
+        password = String.valueOf(passwordTextField.getText());
+        companyPhone = String.valueOf(companyPhoneTextField.getText());
+        HQAddress = String.valueOf(HQAddressTextField.getText());
+
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(companyPhone);
+        System.out.println(HQAddress);
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        scene = new Scene(fxmlLoader.load());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onBackButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register.fxml"));
+        scene = new Scene(fxmlLoader.load());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
