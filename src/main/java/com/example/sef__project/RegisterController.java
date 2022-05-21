41565718a197849c9c715047903633bd7ec2888e
CCXander <xander.cioc@student.upt.ro>
2022-05-21 13:18:33 +0300

    Structuring of files

--- src/main/java/com/example/sef__project/RegisterController.java
+++ src/main/java/com/example/sef__project/RegisterController.java
@@ -3,7 +3,7 @@ package com.example.sef__project;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class RegisterController {
     @FXML
     private Label welcomeText;
 
