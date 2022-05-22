b1349475527a10e4883436347e163ec2ed695200
Theo <theodora.fanica@student.upt.ro>
2022-05-22 17:36:05 +0300

    company

--- src/main/java/Code/Controllers/LoginController.java
+++ src/main/java/Code/Controllers/LoginController.java
@@ -1,5 +1,6 @@
 package Code.Controllers;
 
+import Code.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,9 +10,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
+    @FXML
+    public void clickOnLogIn () throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("company.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
