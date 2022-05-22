137207e56f27fbc8d8594e641b518157dc9762fd
CCXander <103416424+CCXander@users.noreply.github.com>
2022-05-22 14:59:08 +0300

    Login interface in progress

--- src/main/java/Code/Controllers/LoginController.java
+++ src/main/java/Code/Controllers/LoginController.java
@@ -1,5 +1,6 @@
 package Code.Controllers;
 
+import Code.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,4 +15,24 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    public void onLoginClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
+        scene = new Scene(fxmlLoader.load());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void onBackButtonClick(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("register.fxml"));
+        scene = new Scene(fxmlLoader.load());
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
 }
